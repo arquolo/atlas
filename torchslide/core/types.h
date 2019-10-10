4996b5ea0e229e7b9d5c278b6f047515e752af63
Paul M <arquolo@gmail.com>
2019-10-10 19:07:51 +0500

    Add core.types:type_name

--- torchslide/core/types.h
+++ torchslide/core/types.h
@@ -112,6 +112,20 @@ auto make_variant_if(Pred pred, std::variant<Ts...> var) -> std::optional<declty
     return std::nullopt;
 }
 
+template <typename T>
+constexpr std::string_view type_name() {
+    #ifdef __clang__
+        std::string_view p = __PRETTY_FUNCTION__;
+        return std::string_view(p.data() + 34, p.size() - 34 - 1);
+    #elif defined(__GNUC__)
+        std::string_view p = __PRETTY_FUNCTION__;
+        return std::string_view(p.data() + 49, p.find(';', 49) - 49);
+    #elif defined(_MSC_VER)
+        std::string_view p = __FUNCSIG__;
+        return std::string_view(p.data() + 84, p.size() - 84 - 7);
+    #endif
+}
+
 } // namespace ts
 
 // template<typename ...Args>
