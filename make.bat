d189c07bce3c2b79b11e842c043909bbbcb2f211
Paul M <arquolo@gmail.com>
2019-10-31 19:59:32 +0500

    Bump version

--- make.bat
+++ make.bat
@@ -1,3 +1,3 @@
 python setup.py build_ext --force -i
 python setup.py sdist bdist_wheel
-rem twine upload dist\*
+REM twine upload dist\*
--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ class BuildExt(build_ext):
 
 setuptools.setup(
     name=f'{PACKAGE}-any',
-    version='0.0.9',
+    version='0.1.0',
     author='Paul Maevskikh',
     author_email='arquolo@gmail.com',
     url=f'https://github.com/arquolo/{PACKAGE}',
